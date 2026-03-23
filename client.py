6375048018e5008e1b02908003486f760053c2f0
Ana Luísa de Bastos Chagas <161107603+azirablue@users.noreply.github.com>
2026-03-23 16:12:17 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,24 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
+import time
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
-s.close()               # close the connection
+s.connect((HOST, PORT))
+
+while True:
+    msg = input("Digite comando (ou exit): ")
+    if msg == "exit":
+        break
+
+    start_time = time.time()
+
+    s.send(msg.encode())
+    data = s.recv(1024)
+
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    print("Resposta:", data.decode())
+    print(f"Tempo total (cliente): {total_time:.6f}s\n")
+
+s.close()
